0fcdb9cfd9d79325701be98145912df27bbedb17
steve <zhonglinsun517@gmail.com>
2019-06-18 21:42:26 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,33 @@
-Page({})
+const TitleName = {
+  'gn': '',
+  'gj': '',
+  'cj': '',
+  'yl': '',
+  'js': '',
+  'ty': '',
+  'other' : ''
+}
+
+
+Page({
+  data: {
+    title: 'title',
+    detail:'detail',
+    img: '/img/pic.jpg'
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: 'gn',
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        let result = res.data.result
+        console.log(result)
+      }
+    })
+  }, 
+})
